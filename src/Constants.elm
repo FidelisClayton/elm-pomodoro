6c657fecf6b12c8e097c2c574c14876ad396910e
Clayton Fidelis <clayton.fidelis@live.com>
2017-04-15 13:12:25 -0300

    Stop timer when time is over

--- src/Constants.elm
+++ src/Constants.elm
@@ -1,13 +1,9 @@
 module Constants exposing (..)
 
-second : Float
-second = 1000
-
-minute : Float
-minute = 60000
+import Time exposing (second, minute)
 
 socialTime : Float
-socialTime = 5 * minute
+socialTime = 1 * minute
 
 coffeeTime : Float
 coffeeTime = 15 * minute
