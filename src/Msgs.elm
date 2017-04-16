782bc3d5c0191333fcea25689e491cd92e05b47e
Clayton Fidelis <clayton.fidelis@live.com>
2017-04-16 20:36:29 -0300

    Add Notification when time's up

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -14,3 +14,5 @@ type Msg
   | StartWork
   | StartCoffee
   | StartSocial
+  | RequestPermission
+  | UpdateNotificationAccess String
--- src/index.js
+++ src/index.js
@@ -6,7 +6,43 @@ var mountNode = document.getElementById("main")
 
 var app = Elm.Main.embed(mountNode)
 
-app.ports.toJs.subscribe(function (bool) {
-  if (bool)
-    alert("Time is up!")
-})
+function clearFields(object) {
+  const keys = Object.keys(object)
+
+  keys.forEach(function(key) {
+    if (object[key] === null)
+      delete object[key]
+  })
+
+  return object
+}
+
+function requestPermission() {
+  Notification.requestPermission()
+    .then(function(result) {
+      return app.ports.requestPermissionToElm.send(result)
+    })
+}
+
+function notify(options) {
+  options = clearFields(options)
+
+  if (!("Notification" in window)) {
+    return alert("This browser does not support system notifications")
+  }
+
+  else if (Notification.permission === "granted") {
+    return new Notification(options.title, options)
+  }
+
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission(function(permission) {
+      if (permission === "granted") {
+        return new Notification(options.title, options)
+      }
+    })
+  }
+}
+
+app.ports.requestPermissionToJs.subscribe(requestPermission)
+app.ports.notify.subscribe(notify)
